17f17a5d973343874a43ee0b166be287647c0c13
zhengcan <zhengcan@wondersgroup.com>
2020-08-02 16:27:14 +0800

    dev-1.0分支新增功能222

--- src/main/java/enjoy/git/controller/UserController.java
+++ src/main/java/enjoy/git/controller/UserController.java
@@ -22,6 +22,7 @@ public class UserController {
 		System.out.println("测试版本回退V-1.0");
 		System.out.println("测试版本回滚V-1.0");
 		System.out.println("dev-1.0分支新增功能");
+		System.out.println("dev-1.0分支新增功能222");
 		return "This is branch dev-1.0";
 	}
 	
